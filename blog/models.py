f5f47aefe90e37b7d290cbb5fb8cf892b2476e98
Jerin Peter George <jerinpetergeorge@gmail.com>
2021-09-01 22:47:12 +0530

    polls app added

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from accounts.models import User
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "draft"
+        PUBLISHED = "published"
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, choices=Status.choices, default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
