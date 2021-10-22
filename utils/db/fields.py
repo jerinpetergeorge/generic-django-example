e5d1177ffffe1cdb1748d7fe2625819770d3d4ec
Jerin Peter George <jerinpetergeorge@gmail.com>
2021-10-23 00:22:03 +0530

    typo fix

--- utils/db/fields.py
+++ utils/db/fields.py
@@ -6,6 +6,16 @@ from utils.db.functions import get_db_vendor
 class TinyAutoField(SmallAutoField):
     def db_type(self, connection):
         vendor = get_db_vendor()
-        if vendor == "sqlite":
-            return super().db_type(connection=connection)
-        return "tinyint AUTO_INCREMENT"
+        if vendor == "mysql":
+            return "tinyint AUTO_INCREMENT"
+        return super().db_type(connection=connection)
+
+
+def create_large_dataset(size=1000):
+    from kn_dash.factory import PaymentTransactionFactory
+    from kn_dash.models import PaymentTransaction
+    for _ in range(size):
+        PaymentTransaction.objects.bulk_create(
+            PaymentTransactionFactory.build_batch(5000)
+        )
+        print(_)
