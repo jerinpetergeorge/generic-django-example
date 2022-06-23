8d658e9ecd40e575eb37052a0e4b487b438ffbef
Jerin Peter George <jerinpetergeorge@gmail.com>
2022-06-23 13:36:48 +0530

    Set 'tinyint AUTO_INCREMENT' exclusively for MySQL

--- utils/db/fields.py
+++ utils/db/fields.py
@@ -6,6 +6,6 @@ from utils.db.functions import get_db_vendor
 class TinyAutoField(SmallAutoField):
     def db_type(self, connection):
         vendor = get_db_vendor()
-        if vendor == "sqlite":
-            return super().db_type(connection=connection)
-        return "tinyint AUTO_INCREMENT"
+        if vendor == "mysql":
+            return "tinyint AUTO_INCREMENT"
+        return super().db_type(connection=connection)
