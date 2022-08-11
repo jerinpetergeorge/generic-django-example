b3080f4f50d4e3741af035a0f6ab40b6e33acd5c
Jerin Peter George <jerinpetergeorge@gmail.com>
2022-08-11 23:47:00 +0530

    WIP

--- config/settings/serverless.py
+++ config/settings/serverless.py
@@ -5,8 +5,8 @@ INSTALLED_APPS += ["django_s3_sqlite"]
 zappa_sqlite_conf = {
     "ENGINE": "django_s3_sqlite",
     "BUCKET": env("SQLITE_S3_BUCKET", default="generic-django-s3-bucket"),
-    "AWS_S3_ACCESS_KEY": env("AWS_S3_ACCESS_KEY"),
-    "AWS_S3_ACCESS_SECRET": env("AWS_S3_ACCESS_SECRET"),
+    "AWS_S3_ACCESS_KEY": env("AWS_S3_ACCESS_KEY", default=None),
+    "AWS_S3_ACCESS_SECRET": env("AWS_S3_ACCESS_SECRET", default=None),
 }
 db_conf = DATABASES["default"]
 db_conf.update(zappa_sqlite_conf)
